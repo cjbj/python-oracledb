99d49a126626a185c2d55c83ec08b968f4d82250
Christopher Jones <christopher.jones@oracle.com>
2025-10-31 10:09:29 +1100

    CI/CD first attempt

---
+++ .github/scripts/install_oracle_instantclient.sh
@@ -0,0 +1,7 @@
+sudo apt-get install wget libaio1t64
+sudo ln -s /usr/lib/x86_64-linux-gnu/libaio.so.1t64 /usr/lib/x86_64-linux-gnu/libaio.so.1
+wget https://download.oracle.com/otn_software/linux/instantclient/instantclient-basiclite-linuxx64.zip -P /tmp
+sudo unzip /tmp/instantclient-basiclite-linuxx64.zip -d /opt/oracle
+sudo echo /opt/oracle/instantclient* > /etc/ld.so.conf.d/oracle-instantclient.conf
+sudo ldconfig
+sudo echo "DISABLE_OOB=ON" > $(echo /opt/oracle/instantclient*)/network/admin/sqlnet.ora
